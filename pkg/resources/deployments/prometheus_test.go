153017850584f6c2a3bda34e85ee36cc9e65283b
robert.donat <robert.donat@oracle.com>
2021-03-03 08:32:24 -0500

    fix unit tests

--- pkg/resources/deployments/prometheus_test.go
+++ pkg/resources/deployments/prometheus_test.go
@@ -34,8 +34,8 @@ func TestPrometheusDeploymentsNoStorage(t *testing.T) {
 		t.Error(err)
 	}
 
-	assert.Equal(t, 3, len(promDeployment.Spec.Template.Spec.Containers), "Length of generated containers")
-	assert.Equal(t, 3, len(promDeployment.Spec.Template.Spec.Volumes), "Length of generated volumes")
+	assert.Equal(t, 4, len(promDeployment.Spec.Template.Spec.Containers), "Length of generated containers")
+	assert.Equal(t, 7, len(promDeployment.Spec.Template.Spec.Volumes), "Length of generated volumes")
 	assert.Equal(t, 3, len(promDeployment.Spec.Template.Spec.Containers[0].VolumeMounts), "Length of generated mounts for Prometheus node")
 	assert.Equal(t, 0, len(promDeployment.Spec.Template.Spec.Containers[2].VolumeMounts), "Length of generated mounts for Node exporter")
 	assert.Equal(t, 3, len(deployments), "Length of generated deployments")
@@ -65,8 +65,8 @@ func TestPrometheusDeploymentsWithStorage(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	assert.Equal(t, 3, len(promDeployment.Spec.Template.Spec.Containers), "Length of generated containers")
-	assert.Equal(t, 3, len(promDeployment.Spec.Template.Spec.Volumes), "Length of generated volumes")
+	assert.Equal(t, 4, len(promDeployment.Spec.Template.Spec.Containers), "Length of generated containers")
+	assert.Equal(t, 7, len(promDeployment.Spec.Template.Spec.Volumes), "Length of generated volumes")
 	assert.Equal(t, 3, len(promDeployment.Spec.Template.Spec.Containers[0].VolumeMounts), "Length of generated mounts for Prometheus node")
 	assert.Equal(t, 1, len(promDeployment.Spec.Template.Spec.Containers[2].VolumeMounts), "Length of generated mounts for Node exporter")
 	assert.Equal(t, 3, len(deployments), "Length of generated deployments")
