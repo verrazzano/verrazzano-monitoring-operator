6ab494c86e1978f4f18e1272ffd4db9363aa0664
jonathan.maron@oracle.com <jonathan.maron@oracle.com>
2021-04-26 11:12:28 -0400

    ingress work

--- pkg/resources/helper.go
+++ pkg/resources/helper.go
@@ -31,7 +31,7 @@ func GetMetaLabels(vmo *vmcontrollerv1.VerrazzanoMonitoringInstance) map[string]
 
 // GetSpecID returns app label
 func GetSpecID(vmoName string, componentName string) map[string]string {
-	return map[string]string{constants.ServiceAppLabel: vmoName + "-" + componentName, "verazzano.istio.sidecar.enabled": "true"}
+	return map[string]string{constants.ServiceAppLabel: vmoName + "-" + componentName}
 }
 
 // GetServicePort returns service port
