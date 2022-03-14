9dcfc08861677063047ce6d4d7d6a36c00b31431
Anders Swanson <anders.swanson@oracle.com>
2022-03-14 09:24:43 -0700

    Storageclass during update

--- pkg/vmo/statefulset.go
+++ pkg/vmo/statefulset.go
@@ -22,12 +22,12 @@ import (
 
 // CreateStatefulSets creates/updates/deletes VMO statefulset k8s resources
 func CreateStatefulSets(controller *Controller, vmo *vmcontrollerv1.VerrazzanoMonitoringInstance) error {
-	storageClass, err := determineStorageClass(controller, vmo.Spec.StorageClass)
+	storageClass, err := getStorageClassOverride(controller, vmo.Spec.StorageClass)
 	if err != nil {
 		controller.log.Errorf("Failed to determine storage class for VMI %s: %v", vmo.Name, err)
 		return err
 	}
-	statefulSetList, err := statefulsets.New(controller.log, vmo, storageClass.Name)
+	statefulSetList, err := statefulsets.New(controller.log, vmo, storageClass)
 	if err != nil {
 		controller.log.Errorf("Failed to create StatefulSet specs for VMI %s: %v", vmo.Name, err)
 		return err
