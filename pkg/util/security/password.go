6fecbce9c4bb90da2c3a30d1eb708afdda587531
ben.mclean <ben.mclean@oracle.com>
2022-04-01 16:20:43 -0400

    Revert "Revert "generate password""
    This reverts commit 0345d2b453be9cfbfe9f922eb94b600a80900ad0.

---
+++ pkg/util/security/password.go
@@ -0,0 +1,37 @@
+package security
+
+import (
+	"crypto/rand"
+	b64 "encoding/base64"
+	"fmt"
+	"regexp"
+)
+
+//GeneratePassword will generate a password of length
+func GeneratePassword(length int) (string, error) {
+	if length < 1 {
+		return "", fmt.Errorf("cannot create password of length %d", length)
+	}
+	// Enlarge buffer so plenty of room is left when special characters are stripped out
+	b := make([]byte, length*3)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+	pw := b64.StdEncoding.EncodeToString(b)
+	pw, err = makeAlphaNumeric(pw)
+	if err != nil {
+		return "", err
+	}
+	return pw[:length], nil
+}
+
+// makeAlphaNumeric removes all special characters from a password string
+func makeAlphaNumeric(input string) (string, error) {
+	// Make a Regex to say we only want letters and numbers
+	reg, err := regexp.Compile("[^a-zA-Z0-9]+")
+	if err != nil {
+		return "", err
+	}
+	return reg.ReplaceAllString(input, ""), nil
+}
