d4fe64403b4a6d26734a3175e6429a0e280ea0cb
Fred Tibbitts <fred.tibbitts@oracle.com>
2022-04-20 13:23:00 -0400

    Change default role to Editor when auth proxy is disabled

--- pkg/resources/deployments/deployment.go
+++ pkg/resources/deployments/deployment.go
@@ -91,7 +91,7 @@ func New(vmo *vmcontrollerv1.VerrazzanoMonitoringInstance, kubeclientset kuberne
 				{Name: "GF_AUTH_BASIC_ENABLED", Value: "true"},
 				{Name: "GF_USERS_ALLOW_SIGN_UP", Value: "false"},
 				{Name: "GF_USERS_AUTO_ASSIGN_ORG", Value: "true"},
-				{Name: "GF_USERS_AUTO_ASSIGN_ORG_ROLE", Value: "Admin"},
+				{Name: "GF_USERS_AUTO_ASSIGN_ORG_ROLE", Value: "Editor"},
 				{Name: "GF_AUTH_DISABLE_LOGIN_FORM", Value: "false"},
 				{Name: "GF_AUTH_DISABLE_SIGNOUT_MENU", Value: "false"},
 			}...)
