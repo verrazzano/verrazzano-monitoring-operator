68a87b2cfd20115539cccb60c4de2dd2a7ba88da
zaid <zabdulre@usc.edu>
2022-06-23 17:46:13 +0000

    Switched to http endpoint due to istio

--- pkg/vmo/metrics.go
+++ pkg/vmo/metrics.go
@@ -2,7 +2,6 @@ package vmo
 
 import (
 	"net/http"
-	"path"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -11,12 +10,11 @@ import (
 
 // StartMetricsServer starts the metrics endpoint. See StartHTTPsServer for the original implementation from which this function is derived.
 func StartMetricsServer(controller *Controller, certdir string) {
-	//might register collectors here, have not decided yet
 
 	go wait.Until(func() {
 		controller.log.Oncef("Starting metrics server")
 		http.Handle("/metrics", promhttp.Handler())
-		err := http.ListenAndServeTLS(":9100", path.Join(certdir, "tls.crt"), path.Join(certdir, "tls.key"), nil)
+		err := http.ListenAndServe(":9100", nil)
 		if err != nil {
 			controller.log.Errorf("Failed to start metrics server for VMI: %v", err)
 		}
