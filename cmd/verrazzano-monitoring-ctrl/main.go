7b96cafd2a3df14e93a8a112b245b44eba9ef3fb
Zaid Abdulrehman <zabdulre@usc.edu>
2022-06-27 09:12:18 -0700

    Zabdulre/vz 5822 subtask one (#213)
    * Started metrics endpoint in main.go
    
    * Changed endpoint implementation to use http handle
    
    * Switched to http endpoint due to istio
    
    * Code cleanup
    
    * Created metrics pkg
    
    * Created metricsExporter pkg and enabled logs
    
    * Updated third party licenses
    
    * changed pkg name to metricsexporter
    
    * updated third party licenses

--- cmd/verrazzano-monitoring-ctrl/main.go
+++ cmd/verrazzano-monitoring-ctrl/main.go
@@ -14,6 +14,7 @@ import (
 
 	"github.com/verrazzano/verrazzano-monitoring-operator/pkg/config"
 	"github.com/verrazzano/verrazzano-monitoring-operator/pkg/constants"
+	"github.com/verrazzano/verrazzano-monitoring-operator/pkg/metricsexporter"
 	"github.com/verrazzano/verrazzano-monitoring-operator/pkg/util/logs"
 	"github.com/verrazzano/verrazzano-monitoring-operator/pkg/vmo"
 	"go.uber.org/zap"
@@ -61,6 +62,8 @@ func main() {
 	}
 
 	vmo.StartHTTPServer(controller, certdir, port)
+
+	metricsexporter.StartMetricsServer()
 
 	if err = controller.Run(1); err != nil {
 		zap.S().Fatalf("Error running controller: %s", err.Error())
---
+++ pkg/metricsexporter/metricsexporter.go
@@ -0,0 +1,26 @@
+// Copyright (C) 2022, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package metricsexporter
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.uber.org/zap"
+	"k8s.io/apimachinery/pkg/util/wait"
+)
+
+// StartMetricsServer starts the metrics endpoint. See StartHTTPsServer for the original implementation from which this function is derived.
+func StartMetricsServer() {
+
+	go wait.Until(func() {
+		http.Handle("/metrics", promhttp.Handler())
+		err := http.ListenAndServe(":9100", nil)
+		if err != nil {
+			zap.S().Errorf("Failed to start metrics server for VMI: %v", err)
+		}
+	}, time.Second*3, wait.NeverStop)
+
+}
