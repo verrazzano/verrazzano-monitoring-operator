537590f2825e52e1b3c6f6a0f2aa5ff7e606088b
Shashank Kumar <shashank.z.kumar@oracle.com>
2022-06-28 17:11:20 +0000

    refactored

--- pkg/vmo/pvc.go
+++ pkg/vmo/pvc.go
@@ -32,31 +32,31 @@ func CreatePersistentVolumeClaims(controller *Controller, vmo *vmcontrollerv1.Ve
 	if err != nil {
 		return nil, err
 	}
-	emptyStorageClass := &storagev1.StorageClass{}
-	if storageClass == emptyStorageClass {
-		return nil, fmt.Errorf("found an empty storage class")
-	}
 	storageClassInfo := parseStorageClassInfo(storageClass, controller.operatorConfig)
-
+	
 	expectedPVCs, err := pvcs.New(vmo, storageClass.Name)
 	if err != nil {
 		controller.log.Errorf("Failed to create PVC specs for VMI %s: %v", vmo.Name, err)
 		return nil, err
 	}
 	pvcToAdMap := map[string]string{}
-
+	
 	controller.log.Oncef("Creating/updating PVCs for VMI %s", vmo.Name)
-
+	
 	// Get total list of all possible schedulable ADs
 	schedulableADs, err := getSchedulableADs(controller)
 	if err != nil {
 		return pvcToAdMap, err
 	}
-
+	
 	// Keep track of ADs for Prometheus and Elasticsearch PVCs, to ensure they land on all different ADs
 	prometheusAdCounter := NewAdPvcCounter(schedulableADs)
 	elasticsearchAdCounter := NewAdPvcCounter(schedulableADs)
-
+	
+	emptyStorageClass := &storagev1.StorageClass{}
+	if len(expectedPVCs) > 0 && storageClass == emptyStorageClass {
+		return nil, fmt.Errorf("cannot create PVCs when the cluster has no storage class")
+	}
 	for _, expectedPVC := range expectedPVCs {
 		pvcName := expectedPVC.Name
 		if pvcName == "" {
