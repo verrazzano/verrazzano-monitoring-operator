7e4b14e637285348ce9ddd76da090899cd5436be
Anders Swanson <anders.swanson@oracle.com>
2022-09-13 09:18:38 -0700

    Cleanup error messages

--- pkg/vmo/configmap.go
+++ pkg/vmo/configmap.go
@@ -43,9 +43,7 @@ func CreateConfigmaps(controller *Controller, vmo *vmcontrollerv1.VerrazzanoMoni
 	// Only create the CM if it doesnt exist. This will allow us to override the provider file e.g. Verrazzano
 	err := createConfigMapIfDoesntExist(controller, vmo, vmo.Spec.Grafana.DashboardsConfigMap, dashboardTemplateMap)
 	if err != nil {
-		controller.log.Debugf("Failed to create dashboard configmap %s: %v", vmo.Spec.Grafana.DashboardsConfigMap, err)
-		return err
-
+		return controller.log.ErrorfNewErr("Failed to create dashboard configmap %s: %v", vmo.Spec.Grafana.DashboardsConfigMap, err)
 	}
 	configMaps = append(configMaps, vmo.Spec.Grafana.DashboardsConfigMap)
 
@@ -54,14 +52,11 @@ func CreateConfigmaps(controller *Controller, vmo *vmcontrollerv1.VerrazzanoMoni
 		constants.GrafanaTmplAlertManagerURI: resources.GetMetaName(vmo.Name, config.AlertManager.Name)}
 	dataSourceTemplate, err := asDashboardTemplate(constants.DataSourcesTmpl, replaceMap)
 	if err != nil {
-		controller.log.Debugf("Failed to create dashboard datasource template for VMI %s: %v", vmo.Name, err)
-		return err
+		return controller.log.ErrorfNewErr("Failed to create dashboard datasource template for VMI %s: %v", vmo.Name, err)
 	}
 	err = createUpdateDatasourcesConfigMap(controller, vmo, vmo.Spec.Grafana.DatasourcesConfigMap, map[string]string{datasourceYAMLKey: dataSourceTemplate})
 	if err != nil {
-		controller.log.Debugf("Failed to create datasource configmap %s: %v", vmo.Spec.Grafana.DatasourcesConfigMap, err)
-		return err
-
+		return controller.log.ErrorfNewErr("Failed to create datasource configmap %s: %v", vmo.Spec.Grafana.DatasourcesConfigMap, err)
 	}
 	configMaps = append(configMaps, vmo.Spec.Grafana.DatasourcesConfigMap)
 
@@ -96,14 +91,12 @@ func createUpdateDatasourcesConfigMap(controller *Controller, vmo *vmcontrollerv
 
 	existingConfig, err := getConfigMap(controller, vmo.Namespace, configmapName)
 	if err != nil {
-		controller.log.Errorf("Failed to get configmap %s%s: %v", vmo.Namespace, configmapName, err)
 		return err
 	}
 	if existingConfig == nil {
 		configMap := configmaps.NewConfig(vmo, configmapName, data)
 		_, err := controller.kubeclientset.CoreV1().ConfigMaps(vmo.Namespace).Create(context.TODO(), configMap, metav1.CreateOptions{})
 		if err != nil {
-			controller.log.Errorf("Failed to create configmap %s%s: %v", vmo.Namespace, configmapName, err)
 			return err
 		}
 		return nil
@@ -118,7 +111,6 @@ func createUpdateDatasourcesConfigMap(controller *Controller, vmo *vmcontrollerv
 			existingConfig.Data[datasourceYAMLKey] = updatedDatasourceStr
 			_, err := controller.kubeclientset.CoreV1().ConfigMaps(vmo.Namespace).Update(context.TODO(), existingConfig, metav1.UpdateOptions{})
 			if err != nil {
-				controller.log.Errorf("Failed to update configmap %s/%s: %v", vmo.Namespace, configmapName, err)
 				return err
 			}
 		}
@@ -131,7 +123,6 @@ func createConfigMapIfDoesntExist(controller *Controller, vmo *vmcontrollerv1.Ve
 	configMap := configmaps.NewConfig(vmo, configmap, data)
 	_, err := controller.kubeclientset.CoreV1().ConfigMaps(vmo.Namespace).Create(context.TODO(), configMap, metav1.CreateOptions{})
 	if err != nil && !apierrors.IsAlreadyExists(err) {
-		controller.log.Errorf("Failed to create configmap %s/%s: %v", vmo.Namespace, configmap, err)
 		return err
 	}
 	return nil
