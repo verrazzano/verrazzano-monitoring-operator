1411d6364a54de31d78427851f6138b8b5f1eb2e
abnadago <abijitha.nadagouda@oracle.com>
2022-11-16 12:18:08 -0800

    Refactor to osd

--- pkg/config/components.go
+++ pkg/config/components.go
@@ -100,7 +100,7 @@ var Kibana = ComponentDetails{
 
 // OpenSearchDashboards is the default OpenSearchDashboards configuration
 var OpenSearchDashboards = ComponentDetails{
-	Name:              "opensearchdashboards",
+	Name:              "osd",
 	EnvName:           "KIBANA_IMAGE",
 	ImagePullPolicy:   constants.DefaultImagePullPolicy,
 	Port:              5601,
