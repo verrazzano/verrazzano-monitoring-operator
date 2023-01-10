4632ceba3cbf97ff908243c8046a9a261f89335f
abnadago <abijitha.nadagouda@oracle.com>
2023-01-10 08:51:21 -0800

    fix

--- pkg/resources/helper.go
+++ pkg/resources/helper.go
@@ -244,7 +244,7 @@ func GetStorageElementForComponent(vmo *vmcontrollerv1.VerrazzanoMonitoringInsta
 	case config.Grafana.Name:
 		return &vmo.Spec.Grafana.Storage
 	case config.ElasticsearchData.Name:
-		return vmo.Spec.Elasticsearch.DataNode.Storage
+		return vmo.Spec.Opensearch.DataNode.Storage
 	}
 	return nil
 }
@@ -255,7 +255,7 @@ func GetReplicasForComponent(vmo *vmcontrollerv1.VerrazzanoMonitoringInstance, c
 	case config.Grafana.Name:
 		return int32(1)
 	case config.ElasticsearchData.Name:
-		return vmo.Spec.Elasticsearch.DataNode.Replicas
+		return vmo.Spec.Opensearch.DataNode.Replicas
 	}
 	return 0
 }
@@ -564,10 +564,10 @@ func CreateOpenSearchContainerCMD(javaOpts string, plugins []string) string {
 // GIVEN VMI CRD
 // RETURN the list of provided os plugins. If there is no plugins in VMI CRD, empty list is returned.
 func GetOpenSearchPluginList(vmo *vmcontrollerv1.VerrazzanoMonitoringInstance) []string {
-	if vmo.Spec.Elasticsearch.Enabled &&
-		vmo.Spec.Elasticsearch.Plugins.Enabled &&
-		len(vmo.Spec.Elasticsearch.Plugins.InstallList) > 0 {
-		return vmo.Spec.Elasticsearch.Plugins.InstallList
+	if vmo.Spec.Opensearch.Enabled &&
+		vmo.Spec.Opensearch.Plugins.Enabled &&
+		len(vmo.Spec.Opensearch.Plugins.InstallList) > 0 {
+		return vmo.Spec.Opensearch.Plugins.InstallList
 	}
 	return []string{}
 }
