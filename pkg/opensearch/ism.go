915fde8c01165bfecf9e0673a41745a1f239940b
Neeraj Singh <neeraj.n.singh@oracle.com>
2023-02-27 15:33:11 +0530

    updatde

--- pkg/opensearch/ism.go
+++ pkg/opensearch/ism.go
@@ -410,6 +410,7 @@ func (o *OSClient) checkCustomISMPolicyExists(log vzlog.VerrazzanoLogger, opense
 	}
 	for _, policy := range policyList.Policies {
 		if policy.Policy.ISMTemplate[0].Priority == searchPolicy.Policy.ISMTemplate[0].Priority && isItemAlreadyExists(log, policy.Policy.ISMTemplate[0].IndexPatterns, searchPolicy.Policy.ISMTemplate[0].IndexPatterns) {
+			log.Infof("checking.... policy.ID %v and searchPolicy.ID%v ", policy.ID, searchPolicy.ID)
 			if policy.ID == searchPolicy.ID {
 				log.Debugf("VZ created default ISM policy for index pattern %v already exists", searchPolicy.Policy.ISMTemplate[0].IndexPatterns)
 				return false, nil
