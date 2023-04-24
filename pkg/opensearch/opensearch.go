7bd26f4b807135bfe397a22f987ba5d80bd50605
Neeraj Singh <neeraj.n.singh@oracle.com>
2023-04-24 07:15:44 +0530

    VZ-8729: Enhanced condition for ISM (#333)
    * enhanced condition for ISM

--- pkg/opensearch/opensearch.go
+++ pkg/opensearch/opensearch.go
@@ -7,6 +7,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/verrazzano/verrazzano-monitoring-operator/pkg/util/logs/vzlog"
 	"net/http"
 
 	"go.uber.org/zap"
@@ -129,7 +130,7 @@ func (o *OSClient) DeleteDefaultISMPolicy(vmi *vmcontrollerv1.VerrazzanoMonitori
 
 // SyncDefaultISMPolicy set up the default ISM Policies.
 // The returned channel should be read for exactly one response, which tells whether default ISM policies are synced.
-func (o *OSClient) SyncDefaultISMPolicy(vmi *vmcontrollerv1.VerrazzanoMonitoringInstance) chan error {
+func (o *OSClient) SyncDefaultISMPolicy(log vzlog.VerrazzanoLogger, vmi *vmcontrollerv1.VerrazzanoMonitoringInstance) chan error {
 	ch := make(chan error)
 	go func() {
 		if !vmi.Spec.Opensearch.Enabled || vmi.Spec.Opensearch.DisableDefaultPolicy {
@@ -142,7 +143,8 @@ func (o *OSClient) SyncDefaultISMPolicy(vmi *vmcontrollerv1.VerrazzanoMonitoring
 			return
 		}
 		openSearchEndpoint := resources.GetOpenSearchHTTPEndpoint(vmi)
-		_, err := o.createOrUpdateDefaultISMPolicy(openSearchEndpoint)
+		log.Debugf("calling createOrUpdateDefaultISMPolicy")
+		_, err := o.createOrUpdateDefaultISMPolicy(log, openSearchEndpoint)
 		ch <- err
 	}()
 
